91e23a47aca4f155ab4c372350ee036bce9fa0e3
Antonio Muñoz Torres <munoztorres.antonio@gmail.com>
2017-01-13 01:15:20 +0100

    Listado Alumnos

---
+++ controllers/AlumnosController.js
@@ -0,0 +1,9 @@
+function AlumnosController($scope){
+	$scope.alumnos=[
+		{nombre:"Pepito Perez",curso:"1º ESO",telefono:666666666},
+		{nombre:"Juan Lopez",curso:"1º ESO",telefono:777777777},
+		{nombre:"Francisco guitierrez",curso:"3 º ESO",telefono:888888888},
+		{nombre:"Luis Suarez",curso:"4º ESO",telefono:999999999},
+		{nombre:"Jose Monje",curso:"5º ESO",telefono:111111111}
+	];
+}
