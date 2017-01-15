71047a2c22b397db706cb02efdba46d653ebdade
Antonio Muñoz <munoztorres.antonio@gmail.com>
2017-01-15 03:03:05 +0100

    Lista de alumnos y formulario añadir

--- controllers/AlumnosController.js
+++ controllers/AlumnosController.js
@@ -1,9 +1,55 @@
 function AlumnosController($scope){
+	
 	$scope.alumnos=[
-		{nombre:"Pepito Perez",curso:"1º ESO",telefono:666666666},
-		{nombre:"Juan Lopez",curso:"1º ESO",telefono:777777777},
-		{nombre:"Francisco guitierrez",curso:"3 º ESO",telefono:888888888},
-		{nombre:"Luis Suarez",curso:"4º ESO",telefono:999999999},
-		{nombre:"Jose Monje",curso:"5º ESO",telefono:111111111}
+		{id:1,nombre:"Pepito Perez",curso:"1º ESO",telefono:666666666},
+		{id:2,nombre:"Juan Lopez",curso:"1º ESO",telefono:777777777},
+		{id:3,nombre:"Francisco guitierrez",curso:"3 º ESO",telefono:888888888},
+		{id:4,nombre:"Luis Suarez",curso:"4º ESO",telefono:999999999},
+		{id:5,nombre:"Jose Monje",curso:"5º ESO",telefono:111111111}
 	];
+
+	$scope.formVisivility = false; //Por defecto es formulario está oculto
+	                          
+
+	/**
+	 * Añade un nuevoAlumno a la lista de alumnos
+	 * @return none
+	 */
+	$scope.save=function(){
+
+		var nuevoAlumno = {
+			nombre:$scope.nuevoAlumno.nombre,
+			curso:$scope.nuevoAlumno.curso,
+			telefono:$scope.nuevoAlumno.telefono,
+			id:new Date().getTime()
+		}
+
+		$scope.alumnos.push(nuevoAlumno);
+		$scope.hideForm();
+	}
+
+	/**
+	 * Elimina el alumno con idAlumno de los alumnos
+	 * @param  {id del aumno a eliminar}
+	 * 
+	 */
+	$scope.remove=function(idAlumno){
+		var newAlumnos = $scope.alumnos.filter(function(alumno){
+			return alumno.id!=idAlumno;}
+			);
+		$scope.alumnos = newAlumnos;
+	}
+
+
+	/**
+	 * Muestra el formulario
+ 	*/
+	$scope.showForm = function(){
+		$scope.formVisivility = true;
+	}
+
+	$scope.hideForm = function(){
+		$scope.formVisivility = false;
+	}
+	
 }
